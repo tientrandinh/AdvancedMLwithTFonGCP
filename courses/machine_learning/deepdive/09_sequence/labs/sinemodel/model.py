a55906fcb156e6e2c97051db1e9443ebb39cf63f
pl80tech <pl80.tech@gmail.com>
2019-09-23 05:57:08 +0000

    Implement TODO part

--- courses/machine_learning/deepdive/09_sequence/labs/sinemodel/model.py
+++ courses/machine_learning/deepdive/09_sequence/labs/sinemodel/model.py
@@ -38,30 +38,72 @@ def init(hparams):
 
 def linear_model(features, mode, params):
     X = features[TIMESERIES_COL]
-    #TODO: finish linear model
-    pass
+    predictions = tf.layers.dense(X, 1, activation=None)
+    return predictions
+
 
 def dnn_model(features, mode, params):
-  X = features[TIMESERIES_COL]
-  #TODO: finish DNN model
-  pass
+    X = features[TIMESERIES_COL]
+    h1 = tf.layers.dense(X, 10, activation=tf.nn.relu)
+    h2 = tf.layers.dense(h1, 3, activation=tf.nn.relu)
+    predictions = tf.layers.dense(h2, 1, activation=None)  # linear output: regression
+    return predictions
+
 
 def cnn_model(features, mode, params):
-  X = tf.reshape(features[TIMESERIES_COL], [-1, N_INPUTS, 1]) # as a 1D "sequence" with only one time-series observation (height)
-  #TODO: finish CNN model
-  pass
+    X = tf.reshape(features[TIMESERIES_COL],
+                   [-1, N_INPUTS, 1])  # as a 1D "sequence" with only one time-series observation (height)
+    c1 = tf.layers.conv1d(X, filters=N_INPUTS // 2,
+                          kernel_size=3, strides=1,
+                          padding='same', activation=tf.nn.relu)
+    p1 = tf.layers.max_pooling1d(c1, pool_size=2, strides=2)
+
+    c2 = tf.layers.conv1d(p1, filters=N_INPUTS // 2,
+                          kernel_size=3, strides=1,
+                          padding='same', activation=tf.nn.relu)
+    p2 = tf.layers.max_pooling1d(c2, pool_size=2, strides=2)
+
+    outlen = p2.shape[1] * p2.shape[2]
+    c2flat = tf.reshape(p2, [-1, outlen])
+    h1 = tf.layers.dense(c2flat, 3, activation=tf.nn.relu)
+    predictions = tf.layers.dense(h1, 1, activation=None)  # linear output: regression
+    return predictions
+
 
 def rnn_model(features, mode, params):
-  # 1. dynamic_rnn needs 3D shape: [BATCH_SIZE, N_INPUTS, 1]
-  x = tf.reshape(features[TIMESERIES_COL], [-1, N_INPUTS, 1])
-  #TODO: finish rnn model
-  pass
+    CELL_SIZE = N_INPUTS // 3  # size of the internal state in each of the cells
+
+    # 1. dynamic_rnn needs 3D shape: [BATCH_SIZE, N_INPUTS, 1]
+    x = tf.reshape(features[TIMESERIES_COL], [-1, N_INPUTS, 1])
+
+    # 2. configure the RNN
+    cell = tf.nn.rnn_cell.GRUCell(CELL_SIZE)
+    outputs, state = tf.nn.dynamic_rnn(cell, x, dtype=tf.float32)
+
+    # 3. pass rnn output through a dense layer
+    h1 = tf.layers.dense(state, N_INPUTS // 2, activation=tf.nn.relu)
+    predictions = tf.layers.dense(h1, 1, activation=None)  # (?, 1)
+    return predictions
+
 
 # 2-layer RNN
 def rnn2_model(features, mode, params):
-  x = tf.reshape(features[TIMESERIES_COL], [-1, N_INPUTS, 1])
-  #TODO: finish 2-layer rnn model
-  pass
+    # dynamic_rnn needs 3D shape: [BATCH_SIZE, N_INPUTS, 1]
+    x = tf.reshape(features[TIMESERIES_COL], [-1, N_INPUTS, 1])
+
+    # 2. configure the RNN
+    cell1 = tf.nn.rnn_cell.GRUCell(N_INPUTS * 2)
+    cell2 = tf.nn.rnn_cell.GRUCell(N_INPUTS // 2)
+    cells = tf.nn.rnn_cell.MultiRNNCell([cell1, cell2])
+    outputs, state = tf.nn.dynamic_rnn(cells, x, dtype=tf.float32)
+    # 'state' is now a tuple containing the final state of each cell layer
+    # we use state[1] below to extract the final state of the final layer
+    
+    # 3. pass rnn output through a dense layer
+    h1 = tf.layers.dense(state[1], cells.output_size // 2, activation=tf.nn.relu)
+    predictions = tf.layers.dense(h1, 1, activation=None)  # (?, 1)
+    return predictions
+
 
 # create N-1 predictions
 def rnnN_model(features, mode, params):
